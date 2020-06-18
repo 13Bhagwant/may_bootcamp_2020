2a5843a8346063c45bb9a91d68d1e366d44b3cec
Bhagwant Singh <bhagwantrandhawa5@gmail.com>
2020-06-18 14:56:48 -0700

    added migrations in knex_lab

---
+++ labs_for_js_postgres_client/knex_labs/db/migrations/20200618142859_CreatePopulations.js
@@ -0,0 +1,13 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable("populations", (table) => {
+      table.increments("id");
+      table.integer("year");
+      table.integer("quantity");
+      table.bigInteger("country_id").references("countries.id");
+    });
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema.dropTable("populations");
+  };
