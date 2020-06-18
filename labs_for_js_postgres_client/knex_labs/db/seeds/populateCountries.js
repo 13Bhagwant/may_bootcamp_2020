f6ea6b470c6c87457c84dcf270766f07580a3fca
Bhagwant Singh <bhagwantrandhawa5@gmail.com>
2020-06-18 15:13:26 -0700

    added countries with information / Add solution for populate the countries lab

--- labs_for_js_postgres_client/knex_labs/db/seeds/populateCountries.js
+++ labs_for_js_postgres_client/knex_labs/db/seeds/populateCountries.js
@@ -1,12 +1,28 @@
-exports.seed = function(knex) {
+const data = require("../../allCountries");
+
+exports.seed = function (knex) {
+  const tableData = data.reduce((acc, val) => {
+    const lastEntry = acc[acc.length - 1];
+    const newVal = {
+      name: val.countryName,
+      code: val.countryCode,
+    };
+
+    if (
+      lastEntry &&
+      lastEntry.code === val.countryCode &&
+      lastEntry.name === val.countryName
+    ) {
+      return acc;
+    } else {
+      return acc.concat([newVal]);
+    }
+  }, []);
   // Deletes ALL existing entries
-  return knex('countries').del()
+  return knex("countries")
+    .del()
     .then(function () {
       // Inserts seed entries
-      return knex('countries').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      return knex("countries").insert(tableData);
     });
 };
