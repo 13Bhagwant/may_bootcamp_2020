9ea3fd3da13706b18fbc04853faf6c09e52e043d
Bhagwant Singh <bhagwantrandhawa5@gmail.com>
2020-06-25 10:55:33 -0700

    added posts method in postsRouter

--- js_express_and_rest/routes/postsRouter.js
+++ js_express_and_rest/routes/postsRouter.js
@@ -8,4 +8,25 @@ router.get("/new", (request, response) => {
   response.render("posts/new");
 });
 
+// Name: posts#create, method: POST, path: '/posts'
+router.post("/", (request, response) => {
+  const params = request.body;
+  knex("posts")
+    .insert({
+      title: params.title,
+      content: params.content,
+      imageUrl: params.imageUrl,
+    })
+    .returning("*")
+    .then((post) => {
+      // if we want to use a terminating method like
+      // response.send, response.render, or response.redirect,
+      // and we want to do this after inserting something,
+      // updating something, reading something, etc from our
+      // database, we need to use that terminating method
+      // within the callback to '.then'
+      response.send(post);
+    });
+});
+
 module.exports = router;
