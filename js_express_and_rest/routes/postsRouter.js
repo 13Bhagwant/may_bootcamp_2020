7f49b04bfdff789a612ade08c1c7fc5c5474d905
Bhagwant Singh <bhagwantrandhawa5@gmail.com>
2020-06-25 12:22:07 -0700

    added show.ejs to show a single post with id

--- js_express_and_rest/routes/postsRouter.js
+++ js_express_and_rest/routes/postsRouter.js
@@ -10,12 +10,12 @@ router.get("/new", (request, response) => {
 
 // Name: posts#create, method: POST, path: '/posts'
 router.post("/", (request, response) => {
-  const params = request.body;
-  console.log(params);
+  const formData = request.body;
+  console.log(formData);
   //   const title = params.title;
   //   const content = params.content;
   //   const imageUrl = params.imageUrl;
-  const { title, content, imageUrl } = params;
+  const { title, content, imageUrl } = formData;
   knex("posts")
     .insert({
       title,
@@ -31,7 +31,7 @@ router.post("/", (request, response) => {
       // database, we need to use that terminating method
       // within the callback to '.then'
       // response.send(post);
-      response.redirect("/posts");
+      response.redirect(`/posts/${post[0].id}`);
     });
 });
 
@@ -43,6 +43,35 @@ router.get("/", (request, response) => {
     .then((posts) => {
       // response.send(posts);
       response.render("posts/index", { posts: posts });
+    });
+});
+
+// Name: posts#show, method: GET, path: /posts/:id
+router.get("/:id", (request, response) => {
+  // In the URL above, all the words prefixed with ':'
+  // are called URL params. You can view the values of URL params
+  // with the 'request.params' object property. It contains an object
+  // where the property name corresponds to the name of the url param
+  // and it is associated value
+  // 'request.params' is an object with key value pairs created by
+  // pattern-matching against 'variables' named in the URL/path
+  // route/posts/:id/:name/:job the route then accessed was:
+  // /posts/100/Bob/developer
+  // request.params = { id: 100, name: 'bob', job: 'developer' }
+  //   console.log(request.params);
+  const id = request.params.id;
+
+  knex("posts")
+    .where("id", id)
+    .first()
+    .then((post) => {
+      console.log(post);
+      if (post) {
+        // response.send(post);
+        response.render("posts/show", { post: post });
+      } else {
+        response.redirect("/posts");
+      }
     });
 });
 
