ce503e15fbc24cbf6d9c2961a84a6ec108ca1a1f
Bhagwant Singh <bhagwantrandhawa5@gmail.com>
2020-07-02 14:42:38 -0700

    added solutions for sort_book and squared array of arrays

---
+++ ruby_arrays_and_hashes/labs/sort_Book_Names.rb
@@ -0,0 +1,12 @@
+p "Enter Some book Names"
+books = []
+while book_name = gets.chomp
+  p book_name
+  if book_name == 'exit'
+    break
+  else
+    books << book_name.capitalize
+
+  end
+end
+p books.sort
