e6101d1f3765e81964c3d23af31c338737525d31
Bhagwant Singh <bhagwantrandhawa5@gmail.com>
2020-07-06 09:38:20 -0700

    added demo what_is_it

--- ruby_methods_lambdas_and_blocks/demos.rb
+++ ruby_methods_lambdas_and_blocks/demos.rb
@@ -12,7 +12,41 @@ def by_five? num
 end
 
 # To call a method, you can use or not use brackets
-p by_five? (5) # => true 
-p by_five? 6 # => false
+by_five? (5) # => true 
+by_five? 6 # => false
 
 # by_five? # => ArgumentError wrong number of arguments (given 0, expected 1)
+
+
+# Demo: what is it 
+def what_is_it (thing)
+    if [String, Array, Integer].include? (thing.class)
+        thing.class.to_s 
+    else 
+        "Something Else"
+    end
+end
+
+what_is_it "hello"
+what_is_it 1 
+what_is_it [1, 2, 3]
+what_is_it 1.2
+
+# Using return 
+def my_method a, b 
+    return a * b 
+    # As in JavaScript, the lines following 
+    # return are ignored
+    a + b 
+end
+
+my_method 10, 20 # => 200
+my_method 11, 21 # => 231
+
+# Optional/default arguments 
+def multiply (a = 1, b)
+    a * b 
+end
+
+multiply(2) # => 2
+multiply(2, 3) # => 6
