f6eda21e833c28a93ce4c775139ed95120f0e175
Bhagwant Singh <bhagwantrandhawa5@gmail.com>
2020-07-06 14:18:49 -0700

    changed obj to hash and value to key

--- ruby_methods_lambdas_and_blocks/labs/most_common_letter.rb
+++ ruby_methods_lambdas_and_blocks/labs/most_common_letter.rb
@@ -1,21 +1,22 @@
-  def most_common_letter(string)
-    obj = {}
-    string = string.delete(" ")
+def most_common_letter(string)
+    hash = {}
+    string  = string.delete(" ")
     string.each_char do |letter|
-        if obj[letter] == nil
-            obj[letter] = 1
+        if hash[letter] == nil
+            hash[letter] = 1
         else 
-            obj[letter] += 1
+            hash[letter] += 1
         end  
     end
     max = string[0]
-    obj.each_key do |value|
-        if obj[value] > obj[max]
-        max = value
+    hash.each_key do |key|
+        if hash[key] > hash[max]
+            max = key
         end
     end
     puts max
 end
+
 most_common_letter("aaaabbc") # => "a"
 most_common_letter("T a d c g d g d d n") # => "d"
 most_common_letter("1111 monkeys on the wall") # => "1"
